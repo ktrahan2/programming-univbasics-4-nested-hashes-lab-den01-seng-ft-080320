e211849ddcc213022cc4430b16dd705a819de7ac
puckish-software-5970 <ktrahan2@gmail.com>
2020-07-17 17:09:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,8 @@ def changing_alan
         :languages => ["C"]
       }
     }
-	programmer_hash[:alan_key][:known_for] = "GUI"
-	return programmer_hash
+programmer_hash[:alan_key][:known_for] = "GUI"
+return programmer_hash
 end
 
 def adding_to_dennis
